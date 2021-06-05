73033116adb829a33aab3ace4a6e27f0bb45dfb4
Deepak Jain <deepak.jain961@thepsi.com>
2021-06-05 10:32:00 +0530

    added console project

---
+++ InterBaseConnection/Program.cs
@@ -0,0 +1,31 @@
+﻿using InterBaseSql.Data.InterBaseClient;
+using System;
+using System.Data;
+
+namespace InterBaseConnection
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string conString = "Server=localhost;Database=127.0.0.1:\\ProgramData\\Embarcadero\\InterBase\\gds_db\\examples\\database\\employee.gdb;Uid=SYSDBA;password=masterkey";
+            IBConnection connection = new IBConnection(conString);
+            try
+            {
+                if(connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    Console.WriteLine("Connected to InterBase database!");
+                }
+            }
+            catch(IBException ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+        
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# InterBaseConnection
-This is a small program to connect C# with InterBase database using C# native InterBase drivers
